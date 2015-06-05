1d86244b322d3bb98ce48d43bbc6a6ae017c0beb
Frédéric DROUET <fdrouet@exoplatform.com>
2015-06-05 18:39:24 +0200

    Add Ubuntu Trusty (14.04) and fail if the distribution is not managed.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,11 +5,14 @@ class r10k::params {
     'squeeze': {
       $r10k_bin = '/var/lib/gems/1.8/bin/r10k'
     }
-    'precise', 'wheezy': {
+    'precise', 'wheezy', 'trusty': {
       $r10k_bin = '/usr/local/bin/r10k'
     }
     'lucid': {
       $r10k_bin = '/var/lib/gems/1.8/bin/r10k'
+    }
+    default: {
+      fail("The ${module_name} module is not supported on ${::operatingsystem} distribution ${::lsbdistcodename}")
     }
   }
 }
