784588188a7ab866c21a6f79c6c4a44a9e10f173
Frédéric DROUET <fdrouet@exoplatform.com>
2015-09-15 16:46:34 +0200

    Move r10k configuration file to be compliant with 1.5+ version

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,13 @@ class r10k($configfile, $ensure = 'present') {
     provider => gem,
   }
 
+  # this file was deprecated with r10k version 1.5+
+  file { '/etc/r10k.yaml':
+    ensure => absent,
+  }
   file { '/etc/r10k.yaml':
     ensure => present,
+    path => '/etc/puppetlabs/r10k/r10k.yaml',
     owner  => root,
     group  => 0,
     source => $configfile,
